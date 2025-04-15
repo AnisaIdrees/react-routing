7e252aee7082bb7541f971bbeb59ae175c8e89f2
AnisaIdrees <anisaidrees191@gmail.com>
2025-04-15 17:43:16 +0500

    style....

---
+++ src/pages/routing/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+<>
+hellooo
+</>
+  )
+}
+
+export default Header
--- src/pages/routing/Main1.jsx
+++ src/pages/routing/Main1.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Home from './Home'
 import About from './About'
 import Contact from './Contact'
+import Header from './Header'
+import Footer from './Footer'
 
 const Main1 = () => {
   return (
 <>
+<Header/>
 <Home/>
 <About/>
 <Contact/>
+<Footer/>
 </>
   )
 }
