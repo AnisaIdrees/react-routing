1b1ce3f08fbc548355873f0ca7f0d1a864c9d18b
AnisaIdrees <anisaidrees191@gmail.com>
2025-04-16 02:50:58 +0500

    image

--- src/pages/routing/PagesNotFound.jsx
+++ src/pages/routing/PagesNotFound.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 
 const PagesNotFound = () => {
-    // const myStyle={
-    //     color:'red',
-    //     textAlign:'center',
-    //     border:'1px solid blue',
-    //     padding:'20px'
-    // }
+    const myStyle={
+       width:'100%',
+       height:'100vh',
+    } 
   return (
 
   
 <>
 
-<img src='/404.gif' alt="" />
+<img src='/404.gif'  style ={myStyle}alt="" />
 
 </>
   )
