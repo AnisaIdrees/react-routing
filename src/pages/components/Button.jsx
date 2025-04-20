4078dc94c5fe4264b2719724531b72f4ea2c006b
AnisaIdrees <anisaidrees191@gmail.com>
2025-04-20 11:14:21 +0500

    fetch api

---
+++ src/pages/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Button = ({text='Click me'}) => {
+
+  return (
+    <>
+      <button className="Shopbutton">{text}</button>
+    </>
+  );
+};
+
+export default Button;
--- src/pages/routing/Home.jsx
+++ src/pages/routing/Home.jsx
@@ -1,34 +1,38 @@
-import React from 'react'
+import React from "react";
+import Card from "../components/Card";
+import Button from "../components/Button";
 
 const Home = () => {
+  const clothesProduct = async () => {
+    let res = await fetch(
+      "https://dummyjson.com/products/category/womens-dresses"
+    );
+    let data = await res.json();
+    console.log(data.products);
+  };
+  clothesProduct();
 
-const clothesProduct = async ()=>{
-
-  let res = await fetch("https://dummyjson.com/products/category/womens-dresses")
-  let data = await res.json()
-  console.log(data.products);
-
-
-}
-clothesProduct()
-  
   return (
-<>
-<div className="container-xxl hero-sec-home">
-<div className="text-box">
-<p className="text-start fs-2">Smart Products</p>
-<h1 className=" fs-1">Winter Offer <br /> 2025 Collection
-</h1>
-<button className="Shopbutton">
-    Shop Now
-</button>
-</div>
-<div className="image-box">
-<img src="summar-shopping-removebg-preview.png" alt="" />
-</div>
-</div>
-</>
-  )
-}
+    <>
+      <div className="container-xxl hero-sec-home">
+        <div className="text-box">
+          <p className="text-start fs-2">Smart Products</p>
+          <h1 className=" fs-1">
+            Winter Offer <br /> 2025 Collection
+          </h1>
+          <Button text="Shop Now" />
+        </div>
+        <div className="image-box">
+          <img src="summar-shopping-removebg-preview.png" alt="" />
+        </div>
+      </div>
 
-export default Home
+      <div className="home-section2 container-xxl">
+        <h2 className="text-center mt-5 fw-bold">___ DAILY DEALS ! ___</h2>
+        <Card />
+      </div>
+    </>
+  );
+};
+
+export default Home;
